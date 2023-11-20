25f5c07af13c5c7cee61e83122e7c3bfd35c30df
Corentin Deleuse <dlse.corentin@gmail.com>
2023-11-20 16:56:15 +0100

    feat: update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      minHeight : {
+        "header" : "60px",
+        "body" : "calc(100vh - 60px)",
+        "footer" : "60px"
+      },
+      maxWidth : {
+        "container" : "1400px",
+      },
+      colors : {
+        primary : "#1c85e8",
+        dark : "#222222",
+      },
+      backgroundImage : { 
+        "city-background" : "url('../src/assets/city_panorama.jpg')",
+      },
+      fontFamily : {
+        'poppins' : ["Poppins", "sans-serif"],
+      },
+
+    },
   },
   plugins: [],
 }
